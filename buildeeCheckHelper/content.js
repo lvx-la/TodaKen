bfc0fae03be89d186904d186b1df489ee098a063
Yuki's_iMac <s1f101700192@iniad.org>
2021-08-16 21:52:13 +0900

    本番環境で動かないとかいうクソバグ解消（manifest.jsのURLMatchsの末尾に*入れるだけっていう） ローカル用にDLした仮環境で開発していたことが原因と思われ

--- buildeeCheckHelper/content.js
+++ buildeeCheckHelper/content.js
@@ -3,14 +3,27 @@ window.onload = function() {
     var i = 0;
 
     for (let tr of workersTable.rows) {
+        /* -------------- 血圧 ---------------*/
+        var bloodPressureTag = tr.cells[8].children[0];
+        var bloodPressure = bloodPressureTag.textContent.split("～");
+
+        //低血圧はどうしようもないのでとりま高血圧だけ
+        if(bloodPressure[0] >= 170 || bloodPressure[1] >= 90) {
+            workersTable.rows[i].style.backgroundColor = "#ffa07a";
+        }
+
         /* -------------- 年齢 ---------------*/
         var tjb = tr.cells[6];
         var tjbDiv = tjb.querySelector(".shortcut");
         var age = tjbDiv.children[1].textContent.substring(0, 2);
         //高齢者
         if( age >= 65 ){
-            //console.log(age);
             workersTable.rows[i].style.backgroundColor = "#FFD700";
+
+            //高齢かつ高血圧
+            if(bloodPressure[0] >= 170 || bloodPressure[1] >= 90) {
+                workersTable.rows[i].style.backgroundColor = "#dda0dd";
+            }
         //年少者
         }else if( age < 18 ){
             //console.log(age);
@@ -26,14 +39,6 @@ window.onload = function() {
             tr.cells[7].style.fontWeight = "bold";
         }
 
-        /* -------------- 血圧 ---------------*/
-        var bloodPressureTag = tr.cells[8].children[0];
-        var bloodPressure = bloodPressureTag.textContent.split("～");
-
-        //低血圧はどうしようもないのでとりま高血圧だけ
-        if(bloodPressure[0] >= 170 || bloodPressure[1] >= 90) {
-            workersTable.rows[i].style.backgroundColor = "#ffa07a";
-        }
 
         i++;
     }
